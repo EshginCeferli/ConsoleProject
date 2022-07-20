33d50350d6eeaacb5a7208cb23be0c7072fa3f7e
EshginCeferli <eshginij@code.edu.az>
2022-07-20 23:41:06 +0400

    Create, GetById, GetAll,Search methods

--- Repository/Repositories/Interfaces/IRepository.cs
+++ Repository/Repositories/Interfaces/IRepository.cs
@@ -8,7 +8,7 @@ namespace Repository.Repositories.Interfaces
     public interface IRepository<T> where T : BaseEntity
     {
         void Create(T data);
-        void Update(T data);
+        void Update(T data);  
         void Delete(T data);
         T Get(Predicate<T> predicate);
         List<T> GetAll(Predicate<T> predicate);
--- Service/Services/GroupService.cs
+++ Service/Services/GroupService.cs
@@ -2,7 +2,6 @@
 using Repository.Repositories;
 using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Service.Services
 {
